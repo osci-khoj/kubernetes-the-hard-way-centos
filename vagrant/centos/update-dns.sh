41eaff1d68ec974702a698c2641c64b5d7371b22
hojin kim <khoj@osci.kr>
2019-09-19 09:58:32 +0900

    Update update-dns.sh

--- vagrant/centos/update-dns.sh
+++ vagrant/centos/update-dns.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i -e 's/nameserver.*/nameserver 8.8.8.8/' /etc/resolv.conf
-yum install -y systemd-resolved
-sudo systemctl enable systemd-resolved
-sudo systemctl start systemd-resolved
+#yum install -y systemd-resolved
+#sudo systemctl enable systemd-resolved
+#sudo systemctl start systemd-resolved
 yum install -y wget
